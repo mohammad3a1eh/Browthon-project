11c9e9bdfe15e9f042b68fcca3d899bb4d0b18db
Mohammad Saleh <scrolsin333@gmail.com>
2024-03-16 19:11:27 +0330

    style file for css (dark and light)

---
+++ style.py
@@ -0,0 +1,10 @@
+stylesheet = {
+    "dark": {
+        "body": "background-color: #202020;color:white;border: none;",
+        "central_widget": "border: none;background-color: #202020;color:black;",
+    },
+    "light": {
+        "body": "background-color: #f3f3f3;color:black;border: none;",
+        "central_widget": "border: none;background-color: #f3f3f3;color:black;",
+    },
+}
